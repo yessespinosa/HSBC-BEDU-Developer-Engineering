382c9942376c62f514da0cf63bdaa9e933c0ecb8
Yess Espinosa <yessmetal@gmail.com>
2022-12-01 13:59:27 -0600

    Add files via upload

---
+++ 2. Java backend I/02. Patrones de diseño/ComandoRestar.java
@@ -0,0 +1,10 @@
+
+public class ComandoRestar extends ComandoCalculadora implements Comando {
+  public ComandoRestar(float acumulador, float valor) {
+    super(acumulador, valor);
+  }
+
+  public float ejecutar() {
+    return acumulador - valor;
+  }
+}
---
+++ 2. Java backend I/02. Patrones de diseño/ComandoSumar.java
@@ -0,0 +1,10 @@
+
+public class ComandoSumar extends ComandoCalculadora implements Comando {
+  public ComandoSumar(float acumulador, float valor) {
+    super(acumulador, valor);
+  }
+
+  public float ejecutar() {
+    return acumulador + valor;
+  }
+}
